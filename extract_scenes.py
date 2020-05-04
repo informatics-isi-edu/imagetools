6032c928145bb2593bb2677c867f9ee341dc8ffc
Carl Kesselman <carl@isi.edu>
2020-05-04 00:59:35 -0700

    Signficant restructing to work on CZI with multiple scenes, multiple channels and VSI.

--- extract_scenes.py
+++ extract_scenes.py
@@ -1,6 +1,6 @@
 
 import xml.etree.ElementTree as ET
-import tifffile
+#import tifffile
 import os
 import re
 import sys
@@ -8,96 +8,155 @@ import math
 
 import subprocess
 import logging
+logging.basicConfig(level=logging.DEBUG)
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-tiffcomment_cmd = '/opt/local/bin/bftools/tiffcomment'
-bfconvert_cmd = '/opt/local/bin/bftools/bfconvert'
-showinf_cmd = '/opt/local/bin/bftools/showinf'
+bfconvert_cmd = '/usr/local/bin/bftools/bfconvert'
+showinf_cmd = '/usr/local/bin/bftools/showinf'
 vips_cmd = '/usr/local/bin/vips'
 
 magick_cmd = '/usr/local/bin/magick'
 identify_cmd = '/usr/local/bin/identify'
 
-czifile = '/Users/carl/Repos/Projects/imagetools/20160707-hKDCS19_133-JAM-0-30-000.czi'
+imagedir = '/Users/carl/insync/carl@usc.edu/Google Drive/USC/Projects/ImageStuff/'
+
 bf_env = {'BF_MAX_MEM': '10g'}
 
-def czi_to_ome(czifile, overwrite=False):
-    filename, ext = os.path.splitext(czifile)
-    try:
-        os.mkdir(filename)
-    except FileExistsError:
-        pass
-
-    # Convert the entire czi to an omi tiff.
-
-    logger.info('Converting CZI to OME TIF')
-    result = subprocess.run([
-        bfconvert_cmd, '-overwrite' if overwrite else '-nooverwrite', '-noflat', '-bigtiff',
-        czifile, filename + '/' + filename + '.ome.tif'
-    ],
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-        universal_newlines=True,
-        env=bf_env)
-    logger.info(result.stdout)
-    if result.returncode != 0:
-        logger.info('CZI conversion failed')
-        sys.exit(1)
-
-def czi_scenes(filename):
+def image_file_contents(filename, noflat=True):
     """
-    Figure out the number of scenes in the CZI, the starting location, and the number resolutions within the scene.
+    Figure out the structure of the image file including the number of images in the file (.e.g. series) and the
+    names of the channels in the file.
 
     :param filename:
     :return:
     """
 
-    # Figure out the number of scenes in the czi and the starting location for each scene.
-    logger.info("Calculating scene positions....")
-    result = subprocess.run([showinf_cmd, '-nopix', filename], stdout=subprocess.PIPE,
+    # Figure out the number of images in the input file. Noflat option is required so that the series are numbered by
+    # each image stack, and not seperated out one per resolution.  omexml argument is needed as this forces additional
+    # information such as the series name to be put into the text information.
+
+    def map_value(x):
+        if x == 'true':
+            return True
+        elif x == 'false':
+            return False
+        m = re.search('([0-9]+) x ([0-9]+)',x)
+        if m:
+            return (int(m.group(1)), int(m.group(2)))
+        if 'effectively 1' in x:
+            return 1
+        try:
+            if int(x):
+                return int(x)
+        except ValueError:
+           return x
+
+    logger.info("Getting file metadata....")
+    showinf_args = ['-nopix', '-omexml'] + (['-noflat'] if noflat else [])
+
+    result = subprocess.run([showinf_cmd] + showinf_args + [filename], stdout=subprocess.PIPE,
                             universal_newlines=True,
                             env=bf_env, stderr=subprocess.PIPE)
     logger.info(result.stderr)
 
     if result.returncode != 0:
-        print('Scene calculation failed')
-        #sys.exit(1)
+        logger.info('Metadata extraction failed')
+        raise subprocess.CalledProcessError(cmd=showinf_cmd,
+                                            returncode=result.returncode,
+                                            stderr=result.stderr)
 
-    scenes = []
+    images = []
     channels = []
 
+    parsing_series = False
     for i in result.stdout.splitlines():
-        if 'Series count' in i:
-            s = re.search('Series count = ([0-9]+)', i)
-            series_count = int(s.group(1))
-            logger.info("Series count is {}".format(series_count))
-        if 'Scene #' in i:
-            logger.info(i)
-            s = re.search('\|Series ([0-9]+)\|', i)
-            series = int(s.group(1)) - 1
-            scenes.append(series)
+        i = i.lstrip() # Remove formatting characters.
+        if 'Series #' in i:
+            logger.debug(i)
+            series = {}
+            parsing_series = True
+            s = re.search('Series #([0-9]+) -- (.*):', i)
+            series['Number'] = int(s.group(1))
+            series['Name'] = s.group(2)
+            continue
+        if ' = ' in i and parsing_series:
+            logger.debug(i)
+            s = re.search('(.+) = (.+)', i)
+            series[s.group(1)] = map_value(s.group(2))
+        if i == '' and parsing_series:
+            logger.debug(i)
+            # Don't include thumbnails in the output information
+            if not series['Thumbnail series']:
+                images.append(series)
+            parsing_series = False
         if 'Information|Image|Channel|Name' in i:
+            logger.debug(i)
             # Information|Image|Channel
             # Channel names may be in a list or they may be a single channel name.
             s = re.search('Name.*: *\[?(.+)\]?', i)
             channels.extend([i.strip() for i in s.group(1).split(',')])
-        if 'Information|Image|SizeZ' in i:
-            s = re.search('\|SizeZ.*: *\[?([^\]]+)\]?', i)
-            z = int(s.group(1))
+        if '<OME' in i:
+            # Stop once you get to the XML part of the data.
+            break
 
-    resolutions = [v - scenes[c] for c, v in enumerate(scenes[1:])]
-    logger.info('Number of scenes is {}'.format(len(scenes)))
+    # Now get the OME-XML version.
+    metadata = ET.fromstring(result.stdout[result.stdout.find('<OME'):])
 
-    # The pyramids in the last scene is determined by the total number of images, leaving out the thumbnail and label.
-    if len(scenes) == 0:
-        scenes = [0]
-    resolutions.append((series_count - 2) - scenes[-1])
-    return scenes, resolutions, channels, z
+    logger.info('Number of series is {}'.format(len(images)))
 
-def seadragon_tiffs(czifile, overwrite=False, delete_ome=True):
+    # Calculate what the corrisponding series number would be if the noflat option was not used.
+    resolutions = images[0].get('Resolutions', 1)
+    offset = 0
+    for i in images:
+        i['Flattened series'] = offset
+        offset = offset + resolutions
 
-    filename, ext = os.path.splitext(czifile)
+    if len(channels) < images[0]['SizeC']:
+        channels.extend(['Channel {}'.format(i) for i in range(images[0]['SizeC'] - len(channels))])
+
+    return images, channels, metadata
+
+
+def ome_tiff_filename(file, series, channel, z):
+    return '{}-{}-{}-{}.ome.tif'.format(file,
+                                        series['Number'] if series is not None else "%s",
+                                        channel if channel is not None else (
+                                            0 if series['SizeC'] == 1 else '%c'),
+                                        z if z is not None else (
+                                            0 if series['SizeZ'] == 1 else '%z'))
+
+
+def split_tiff(imagefile, ometiff_file, series=None, z=None, channel=None, overwrite=False):
+
+    # Create template of output file.  If Z or Channel are none, use the implicit file splitting in bfconvert.
+    # If a value for C or Z is not provided and there is only one channel or Z plane, then omit argument, otherwise
+    # use the %c or %z to get bfconvert to do the splitting and filenaming.
+    split_ome_tiff = ome_tiff_filename(ometiff_file, series, channel, z)
+    bfconvert_args = ['-overwrite' if overwrite else '-nooverwrite']
+    if series is not None:
+        bfconvert_args.extend(['-series', str(series['Flattened series'])])
+    if channel is not None and (series['SizeC'] >1):
+        bfconvert_args.extend(['-channel', str(channel)])
+    if z is not None and (series['SizeZ'] > 1):
+       bfconvert_args.extend(['-z', str(z)])
+
+    result = subprocess.run([bfconvert_cmd] + bfconvert_args + [imagefile, split_ome_tiff],
+                            env=bf_env, check=True,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT, universal_newlines=True)
+    logger.info(result.stdout)
+
+    if result.returncode != 0:
+        logger.info('Tiff extraction failed')
+        raise subprocess.CalledProcessError(cmd=showinf_cmd,
+                                            returncode=result.returncode,
+                                            stderr=result.stderr)
+
+def seadragon_tiffs(image_path, overwrite=False, delete_ome=False):
+    image_file = os.path.basename(image_path)
+    filename, ext = os.path.splitext(image_file)
     try:
         os.mkdir(filename)
     except FileExistsError:
@@ -105,51 +164,35 @@ def seadragon_tiffs(czifile, overwrite=False, delete_ome=True):
 
     filename = filename + '/' + filename
 
-    scenes, pyramids, channels, z = czi_scenes(czifile)
-
-    # Pick the slice in the middle, if there is a Z stack.
-    z_plane = int(math.ceil(z/2) - 1)
+    series_list, channel_list, _ = image_file_contents(image_path)
 
     # Create a non-ome tiff pyramid version of the file optimized for open sea dragon.
-    for scene, series in enumerate(scenes):
-        for channel, channel_name in enumerate(channels):
-            logger.info('Splitting out scene Scene:{} Series:{} Channel: {}|{} Z:{} to OME TIFF'.format(scene, series, channel, channel_name, z_plane))
-            # First we pull out the single image that is the specified channel from the scene at highest resolution.
-            # This will be the first image in the series.
+    for series in series_list:
+        # Pick the slice in the middle, if there is a Z stack.
+        z_plane = int(math.ceil(series['SizeZ'] / 2) - 1)
+        split_tiff(image_path, filename, series=series, z=z_plane, overwrite=overwrite)
 
-            ome_tiff = '{}-{}-{}-{}.ome.tif'.format(filename, scene, channel, z_plane)
+        # Now convert this single image to a pyramid with 256x256 jpeg compressed tiles, which is going to be
+        # good for openseadragon.
+        for channel in  range(series['SizeC']):
+            logger.info('Converting scene {} {} {} to compressed TIFF'.format(series['Number'], channel, z_plane))
 
-            result = subprocess.run([bfconvert_cmd,
-                                     '-series', str(scene),
-                                     '-z', str(z_plane),
-                                     '-overwrite' if overwrite else '-nooverwrite'
-                                     ] +
-                                    # Seems that bfconvert doesn't like asking for a channel if there is only one....
-                                    (['-channel', str(channel)] if len(channels) > 1 else []) +
-                                    [czifile, ome_tiff],
-                                    env=bf_env, check=True,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.STDOUT, universal_newlines=True)
-            logger.info(result.stdout)
-
-            logger.info('Converting scene {} {} {} to compressed TIFF'.format(series, channel, z_plane))
-
-            # Now convert this single image to a pyramid with 256x256 jpeg compressed tiles, which is going to be
-            # good for openseadragon.
             vips_convert = [
                 vips_cmd, 'tiffsave',
-                ome_tiff,
-                '{}-{}-{}.tif'.format(filename, scene, channel_name.replace(' ','_')),
+                ome_tiff_filename(filename, series, channel, z_plane),
+                '{}-{}-{}.tif'.format(filename, series['Number'], channel_list[channel].replace(' ', '_')),
                 '--tile', '--pyramid', '--compression', 'jpeg',
                 '--tile-width', '256', '--tile-height', '256'
             ]
 
             result = subprocess.run(vips_convert, check=True,
-                                    stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+                            stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
             logger.info(result.stdout)
-            if delete_ome:
-                os.remove(ome_tiff)
+            if result.returncode != 0:
+                logger.info('Tiff extraction failed')
 
+            if delete_ome:
+                pass
 
 def czi_coords(file):
 
@@ -182,8 +225,8 @@ def czi_coords(file):
     return coords
 
 
-def main(czifile, overwrite=False):
-    seadragon_tiffs(czifile, overwrite)
+def main(imagefile, overwrite=False):
+    seadragon_tiffs(imagefile, overwrite=overwrite)
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1]))
